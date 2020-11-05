968f3820c948b4c7028e2a608101b430718d2a4d
ShubhamGaikar <73989886+ShubhamGaikar@users.noreply.github.com>
2020-11-05 17:43:53 +0530

    Create Pract1.java

---
+++ Pract1.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class prac1{
+	public static void main(String[] args){
+		Scanner inp = new Scanner(System.in);
+		int a,b;
+		System.out.println("Addition of numbers");
+		a = inp.nextInt();
+		b = inp.nextInt();
+		System.out.println("Sum: " +(a+b));
+		}
+	}
