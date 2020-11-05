eecff045bc04d1e2491adf149d5565102eca0f89
ShubhamGaikar <73989886+ShubhamGaikar@users.noreply.github.com>
2020-11-05 17:44:39 +0530

    Create Pract2.java

---
+++ Pract2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class prac2{
+	public static void main(String[] args){
+		Scanner inp = new Scanner(System.in);
+		System.out.println("Predict the result");
+		System.out.println("Enter the marks of the student: ");
+		int m = inp.nextInt();
+		if(m>=40){
+			System.out.println("Pass");
+			}else{System.out.println("Fail");}
+		}
+	}
